ca795246e12e670784f459dd34ce14ad15b8b38a
Alex <sturmmanngr@gmail.com>
2024-04-15 09:47:31 +0300

    temp defines

---
+++ temp_gwr/common/defines/temp_tgwr_defines.lua
@@ -0,0 +1,5 @@
+
+-- TEMP SUBMOD
+NDefines.NMilitary.UNIT_LEADER_USE_NONLINEAR_XP_GAIN = false  
+NDefines.NProduction.BASE_FACTORY_SPEED_MIL = 4.50 				-- Base factory speed multiplier (how much hoi3 style IC each factory gives).
+NDefines.NProduction.BASE_FACTORY_SPEED_NAV = 4.50 	
